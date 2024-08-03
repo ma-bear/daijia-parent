545aa1f4c5437e643bd0b8cd394b960b3b8313b6
葫芦给 <2992374334@qq.com>
2024-08-03 17:02:50 +0800

    优惠卷领取(sql编写)

--- service/service-coupon/src/main/java/com/atguigu/daijia/coupon/service/impl/CouponInfoServiceImpl.java
+++ service/service-coupon/src/main/java/com/atguigu/daijia/coupon/service/impl/CouponInfoServiceImpl.java
@@ -1,15 +1,101 @@
 package com.atguigu.daijia.coupon.service.impl;
 
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
 import com.atguigu.daijia.coupon.mapper.CouponInfoMapper;
+import com.atguigu.daijia.coupon.mapper.CustomerCouponMapper;
 import com.atguigu.daijia.coupon.service.CouponInfoService;
 import com.atguigu.daijia.model.entity.coupon.CouponInfo;
+import com.atguigu.daijia.model.entity.coupon.CustomerCoupon;
+import com.atguigu.daijia.model.vo.base.PageVo;
+import com.atguigu.daijia.model.vo.coupon.NoReceiveCouponVo;
+import com.atguigu.daijia.model.vo.coupon.NoUseCouponVo;
+import com.atguigu.daijia.model.vo.coupon.UsedCouponVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponInfo> implements CouponInfoService {
 
+    @Autowired
+    private CouponInfoMapper couponInfoMapper;
+
+    @Override
+    public PageVo<NoReceiveCouponVo> findNoReceivePage(Page<CouponInfo> pageParam, Long customerId) {
+        IPage<NoReceiveCouponVo> pageInfo = couponInfoMapper.findNoReceivePage(pageParam, customerId);
+        return new PageVo(pageInfo.getRecords(), pageInfo.getPages(), pageInfo.getTotal());
+    }
+
+    @Override
+    public PageVo<NoUseCouponVo> findNoUsePage(Page<CouponInfo> pageParam, Long customerId) {
+        IPage<NoUseCouponVo> pageInfo = couponInfoMapper.findNoUsePage(pageParam, customerId);
+        return new PageVo(pageInfo.getRecords(), pageInfo.getPages(), pageInfo.getTotal());
+    }
+
+    @Override
+    public PageVo<UsedCouponVo> findUsedPage(Page<CouponInfo> pageParam, Long customerId) {
+        IPage<UsedCouponVo> pageInfo = couponInfoMapper.findUsedPage(pageParam, customerId);
+        return new PageVo(pageInfo.getRecords(), pageInfo.getPages(), pageInfo.getTotal());
+    }
+
+    @Autowired
+    private CustomerCouponMapper customerCouponMapper;
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public Boolean receive(Long customerId, Long couponId) {
+        //1、查询优惠券
+        CouponInfo couponInfo = this.getById(couponId);
+        if(null == couponInfo) {
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
+
+        //2、优惠券过期日期判断
+        if (couponInfo.getExpireTime().before(new Date())) {
+            throw new GuiguException(ResultCodeEnum.COUPON_EXPIRE);
+        }
+
+        //3、校验库存，优惠券领取数量判断
+        if (couponInfo.getPublishCount() !=0 && couponInfo.getReceiveCount() >= couponInfo.getPublishCount()) {
+            throw new GuiguException(ResultCodeEnum.COUPON_LESS);
+        }
+
+        //4、校验每人限领数量
+        if (couponInfo.getPerLimit() > 0) {
+            //4.1、统计当前用户对当前优惠券的已经领取的数量
+            long count = customerCouponMapper.selectCount(new LambdaQueryWrapper<CustomerCoupon>().eq(CustomerCoupon::getCouponId, couponId).eq(CustomerCoupon::getCustomerId, customerId));
+            //4.2、校验限领数量
+            if (count >= couponInfo.getPerLimit()) {
+                throw new GuiguException(ResultCodeEnum.COUPON_USER_LIMIT);
+            }
+        }
+
+        //5、更新优惠券领取数量
+        int row = couponInfoMapper.updateReceiveCount(couponId);
+        if (row == 1) {
+            //6、保存领取记录
+            this.saveCustomerCoupon(customerId, couponId, couponInfo.getExpireTime());
+            return true;
+        }
+        throw new GuiguException(ResultCodeEnum.COUPON_LESS);
+    }
+
+    private void saveCustomerCoupon(Long customerId, Long couponId, Date expireTime) {
+        CustomerCoupon customerCoupon = new CustomerCoupon();
+        customerCoupon.setCustomerId(customerId);
+        customerCoupon.setCouponId(couponId);
+        customerCoupon.setStatus(1);
+        customerCoupon.setReceiveTime(new Date());
+        customerCoupon.setExpireTime(expireTime);
+        customerCouponMapper.insert(customerCoupon);
+    }
 
 }
